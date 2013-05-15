ee97515bd47ac033e5a9c4db67f1c5ca2ce620b0
Izhar Firdaus <izhar@inigo-tech.com>
2013-05-15 14:28:07 +0800

    added upgrade profile for rawhtml

--- wcc/rawhtml/upgrades/handlers.py
+++ wcc/rawhtml/upgrades/handlers.py
@@ -2,3 +2,16 @@ from collective.grok import gs
 from Products.CMFCore.utils import getToolByName
 
 # -*- extra stuff goes here -*- 
+
+
+@gs.upgradestep(title=u'Upgrade wcc.rawhtml to 1000',
+                description=u'Upgrade wcc.rawhtml to 1000',
+                source='1', destination='1000',
+                sortkey=1, profile='wcc.rawhtml:default')
+def to1000(context):
+    setup = getToolByName(context, 'portal_setup')
+    setup.runAllImportStepsFromProfile('profile-wcc.rawhtml.upgrades:to1000')
+    catalog = getToolByName(context, 'portal_catalog')
+    for brain in catalog(portal_type='wcc.rawhtml.rawhtml'):
+        obj = brain.getObject()
+        obj.reindexObject()
--- wcc/rawhtml/upgrades/profiles.py
+++ wcc/rawhtml/upgrades/profiles.py
@@ -1,3 +1,8 @@
 from collective.grok import gs
 
 # -*- extra stuff goes here -*- 
+
+gs.profile(name=u'to1000',
+           title=u'Upgrade wcc.rawhtml to 1000',
+           description='',
+           directory='profiles/to1000')
